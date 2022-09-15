42a01ba511e958288ad4c7fdb8ab6afeb7ff9e82
LEONARDO FERREIRA NASCIMENTO DA SILVA <RM20202930045@etsacad.local>
2022-09-15 09:27:10 -0300

    beta logic to convert db in json

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from ui_main import Ui_MainWindow
 from ui_addCards import Ui_AddCardsWindow
 from ui_studyCards import Ui_StudyCardsWindow
 
+import pandas as pd
+import json
+
 class Window(QWidget):
     def __init__(self):
         super(Window, self).__init__()
@@ -51,6 +54,8 @@ class Window(QWidget):
         widgets.tblWidgetTopics.cellWidget(lastrow, 0).clicked.connect(self.addDeck)
 
         print("="*50)
+        
+        self._to_json()
 
     def loadWidgetCell(self, tablerow):
         btnStartStudy, btnAddCards = None, None
@@ -178,6 +183,29 @@ class Window(QWidget):
         return cardsRecords, cardsCount
 
     #######################
+    
+    # JSON CONVERT FUNCTIONS
+    
+    
+    def _to_json(self):
+        """Convert a sqlite3 database to json"""
+        with FlashcardsDB() as db:
+            results = db.cursor.execute("SELECT * from topics")
+            rows = [row for row in results]
+            cols = [col for col in results.description]
+ 
+        topics = []
+        for row in rows:
+            topic = {}
+            for col_name, val in zip(cols, row):
+                topic[col_name] = val
+            topics.append(row)
+            
+        with open('topics.json', 'w') as json_file:
+            topicsJSON = json.dumps(topics, indent=4)
+            print(f"topics in JSON: {topicsJSON}")
+            json.dump(topicsJSON, json_file)
+    
 
 if __name__ == "__main__":
     import sys
